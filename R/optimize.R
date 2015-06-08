cb3524f60fd6d903fa91c3b75c104b5c551f6f5a
TobiasMadsen <tobias.madsen@gmail.com>
2015-06-08 12:53:19 +0200

    BetaOptimizer use range. Better output from optimization functions.

--- R/optimize.R
+++ R/optimize.R
@@ -3,6 +3,7 @@
 #################################################
 
 #' Beta optimization
+#' @param range     range of variable
 #' @export
 betaOptimize <- function(range = c(0,1)){
   # Return a function that takes expcounts and return 
@@ -10,9 +11,19 @@ betaOptimize <- function(range = c(0,1)){
 }
 
 #' Linear Regression optimization
+#' Optimize a normal linear regression
+#' @param range1    range of independent variable
+#' @param range2    range of dependent variable
 #' @export
-linregOptimize <- function(range1 = 1:100, range2 = 1:100){
+linregOptimize <- function(range1 = c(0,100), range2 = c(0,100) ){
   return(function(expCounts){.linregOptimize(expCounts, range1 = range1, range2 = range2)})
+}
+
+#' Normal optimization
+#' @param range     range of variable
+#' @export
+normOptimize <- function(range = c(0,100)){
+  return(function(expCounts){.normOptimize(expCounts, range = range)})
 }
 
 #################################################
@@ -20,11 +31,11 @@ linregOptimize <- function(range1 = 1:100, range2 = 1:100){
 # Take expectation counts and return a potential
 #################################################
 
-.linregOptimize <- function(expCounts, range1 = 1:nrow(expCounts), range2 = 1:ncol(expCounts)){
+.linregOptimize <- function(expCounts, range1 = c(1,nrow(expCounts)), range2 = c(1,ncol(expCounts))){
   # Ranges are only used for output
   SP_xy <- sum(1:nrow(expCounts) %*% t(1:ncol(expCounts)) * expCounts)
-  S_x   <- sum(1:nrow(expCounts) %*% t(rep(1,ncol(expCounts))) * expCounts)
-  S_y   <- sum(rep(1,nrow(expCounts)) %*% t(1:ncol(expCounts)) * expCounts)
+  S_x   <- sum(1:nrow(expCounts) %*% t(rep(1,ncol(expCounts))) * expCounts) # x runs along rows
+  S_y   <- sum(rep(1,nrow(expCounts)) %*% t(1:ncol(expCounts)) * expCounts) # y runs along columns
   USS_x <- sum( (1:nrow(expCounts))**2 %*% t(rep(1,ncol(expCounts))) * expCounts)
   USS_y <- sum(rep(1,nrow(expCounts)) %*% t((1:ncol(expCounts))**2) * expCounts)
   N <- sum(expCounts)
@@ -34,7 +45,7 @@ linregOptimize <- function(range1 = 1:100, range2 = 1:100){
   SPD_xy <- SP_xy - S_x*S_y/N
   
   alpha <- SPD_xy / SSD_x
-  beta  <- (S_y - S_x* alpha) / N
+  beta  <- (S_y - S_x* alpha) / N - 1/2
   var   <- (SSD_y - SPD_xy * SPD_xy/SSD_x) / N
   if(var  <= 0)
     stop("Variance 0 or less")
@@ -43,9 +54,14 @@ linregOptimize <- function(range1 = 1:100, range2 = 1:100){
     diff( pnorm(c(-Inf, 2:ncol(expCounts), Inf) ,x*alpha+beta+0.5, sqrt(var)) )
   }))
   
+  # Scale to range
+  alphaScaled <- alpha / (diff(range1)/nrow(expCounts)) * (diff(range2)/ncol(expCounts))
+  betaScaled <- beta * (diff(range2)/ncol(expCounts)) + range2[1]
+  varScaled <- var * (diff(range2)/ncol(expCounts))**2
+  
   str <- "Linear Regression Potential\n"
-  str <- paste0(str, "alpha:\t", signif(alpha, 5), "\nbeta:\t", signif(beta, 5), '\nvar:\t', signif(var, 5), '\n')
-  return(list(pot = pot, str = str))    
+  str <- paste0(str, "alpha:\t", signif(alphaScaled, 5), "\nbeta:\t", signif(betaScaled, 5), '\nvar:\t', signif(varScaled, 5), '\n')
+  return(list(pot = pot, str = str, alpha = alphaScaled, beta = betaScaled, var = varScaled))    
 }
 
 .rowOptimize <- function(expCounts){
@@ -58,35 +74,46 @@ linregOptimize <- function(range1 = 1:100, range2 = 1:100){
   return(list(pot = pot, str = str))
 }
 
-.normOptimize <- function(expCounts, range = 1:ncol(expCounts)){
+.normOptimize <- function(expCounts, range = c(0,ncol(expCounts))){
   str <- "norm-potential update\n"
   i <- 0
+  meanScaled <- rep(0, nrow(expCounts))
+  varScaled <- rep(0, nrow(expCounts))
   pot <- t(apply(expCounts, 1, FUN=function(x){
     xs <- sum(x*seq_along(x) )
-    xm <- xs/sum(x)+1/2
+    xm <- xs/sum(x)-1/2
     xv <- (sum(x*seq_along(x)**2)-xs**2/sum(x))/sum(x)
     i <<- i + 1
-    str <<- paste0(str, i, "\tmean:\t", signif(xm, 5), "\tvar:\t", signif(xv, 5), '\n')
+    
+    meanScaled[i] <<- xm * (diff(range)/ncol(expCounts)) + range[1]
+    varScaled[i]  <<- xv * (diff(range)/ncol(expCounts))**2
+    str <<- paste0(str, i, "\tmean:\t", signif(meanScaled[i], 5), "\tvar:\t", signif(varScaled[i], 5), '\n')
     diff(pnorm( c(-Inf, 2:length(x), Inf), xm, sqrt(xv)))
   }))
-  return(list(pot = pot, str = str))
+  return(list(pot = pot, str = str, means = meanScaled, vars = varScaled))
 }
 
 .betaOptimize <- function(expCounts, range = c(0,1)){
   # Method of moments
   str <- "beta-potential update\n"
   i <- 0
+  val <- ((1:ncol(expCounts)-0.5)/ncol(expCounts))*diff(range)+range[1]
+  breaks <- seq(range[1], range[2], length.out = ncol(expCounts)+1 )
+  alphas <- rep(0, nrow(expCounts))
+  betas <- rep(0, nrow(expCounts))
+  
   pot <- t(apply(expCounts, 1, FUN=function(x){
-    val <- (seq_along(x)-0.5)/length(x)
     xs  <- sum(x*val )
     xm  <- xs/sum(x)
     xv  <- (sum(x*val**2)-xs**2/sum(x) )/sum(x)
     a   <- xm*(xm*(1-xm)/xv-1)
     b   <- a*(1/xm-1)
-    
     i <<- i + 1
+    
+    alphas[[i]] <<- a
+    betas[[i]]  <<- b
     str <<- paste0(str, i, "\talpha:\t", signif(a, 5), "\tbeta:\t", signif(b, 5), '\n')
-    diff(pbeta( 0:length(x)/length(x), a, b))
+    diff(pbeta( breaks, a, b))
   }))
-  return(list(pot = pot, str = str))
+  return(list(pot = pot, str = str, alphas = alphas, betas = betas))
 }
--- tests/testthat/test_optimize.R
+++ tests/testthat/test_optimize.R
@@ -9,9 +9,73 @@ test_that("Beta optimizer",{
   expCounts <- matrix(table(c(discrete_dat, 1:100))-1, 1, 100)
   
   # Optimize
-  pot <- betaOptimize(range = c(0,0.1))(expCounts)$pot
+  betaOpt <- betaOptimize(range = c(0,0.1))(expCounts)
+  pot <- betaOpt$pot
   
+  # Tests
   expect_less_than(
     max(abs(pbeta(1:100/1000, 4, 140) - cumsum(pot[1,]))),
-    0.05)
+    0.005)
+  
+  expect_true( 4*0.95 < betaOpt$alphas[1] && betaOpt$alphas[1] < 4*1.05 )
+  expect_true( 140*0.95 < betaOpt$betas[1] && betaOpt$betas[1] < 140*1.05 )
+})
+
+test_that("Linreg optimizer 1",{
+  # Generate data
+  dat <- matrix(c(0,0,1,0,0,
+                  0,1,2,0,0,
+                  1,2,1,0,0,
+                  2,1,0,0,0,
+                  1,0,0,0,0), 5,5)
+  
+  # Optimize
+  linreg <- linregOptimize(range1 = c(0.5,5.5), range2 = c(0.5,5.5))(dat)
+  
+  # Tests
+  expect_equal(linreg$alpha, -1)
+  expect_equal(linreg$beta, 5)
+  expect_equal(linreg$var, 0.5)
+})
+
+test_that("Linreg optimizer 2",{
+  # Test scaling
+  # Generate data
+  dat <- matrix(c(0,0,1,0,
+                  0,1,2,0,
+                  1,2,1,0,
+                  2,1,0,0,
+                  1,0,0,0), 4,5)
+  
+  # Optimize
+  # Bins x corresponds to 1,2,3,4
+  # Bins y corresponds to 1,2,3,4,5
+  linreg <- linregOptimize(range1 = c(0.5,4.5), range2 = c(0.5,5.5))(dat) 
+  
+  # Tests
+  expect_equal(linreg$alpha, -1)
+  expect_equal(linreg$beta, 5)
+  expect_equal(linreg$var, 0.5)
+  
+  # Optimize
+  # Bins x corresponds to 2,4,6,8
+  # Bins y corresponds to 3,7,11,15,19
+  linreg <- linregOptimize(range1 = c(1,9), range2 = c(1,21))(dat)
+  
+  # Tests
+  expect_equal(linreg$alpha, -2)
+  expect_equal(linreg$beta, 19)
+  expect_equal(linreg$var, 8)
+})
+
+test_that("Normal optimizer",{
+  # Generate data
+  dat <- matrix(c(0,0,1,2,1,0), 1, 6)
+  
+  # Optimize
+  normopt <- normOptimize(range = c(1,13))(dat)
+  
+  # Tests
+  expect_equal(normopt$means[1], 8)
+  expect_equal(normopt$vars[1], 2)
 })
